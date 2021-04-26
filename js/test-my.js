b06868ae4dc0ac40c073484dddaa0d52b7032678
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-04-26 22:33:22 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -1,8 +1,9 @@
 console.log("test my here!");
 $(function() {
 $(".demo1").addClass("slider-nav");
-$(".section_vebirars").addClass("slider-for");
 });
+
+$(".section_vebirars").addClass("slider-for");
 
 
 /* СТАРТ */
