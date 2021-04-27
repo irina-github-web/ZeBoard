e3e6a0ee6bbdafd01a79081e292909e697d473e9
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-04-27 22:21:12 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -84,7 +84,7 @@ $.getJSON("https://video.gd.ru/event-type-2", function (data) {
       "slickAdd",
       '<div class="chapter_card chapter1">' +
         '<a href="#vebirars">' +
-        `<div class="chapter1_img" style="background-image: url(${item[i].video_type_pic_url});"></div>` +
+        `<div class="chapter1_img" style="background-image: url(${item.video_type_pic_url});"></div>` +
         '<div class="chapter_txt">' +
         `<h3 class="heading_small">${item.video_type_name}</h3>` +
         '<div class="razdelit_small"></div>' +
