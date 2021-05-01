4fae8b1c8b5b2738e91e87996fe6c33baef1575d
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-01 12:17:51 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -114,13 +114,13 @@ $.getJSON("https://video.gd.ru/event-type-2", function (data) {
           // alert(item["videos"][iVideoIdFirst]["video_name"]); 
 
           `<a href=${iVideoIdFirst.video_url} class="fancybox-media w-inline-block">` +
-            `<div class="video" style="background-color:rgba(63, 145, 229, 0.7);background-image: url(${item["videos"][iVideoIdFirst]["video_pic_url"]});background-size:cover;"><img src="images/icn_play_big.png" alt="" class="icn_big_play">` +
+            `<div class="video" style="background-color:rgba(63, 145, 229, 0.7);background-image: url(${iVideoIdFirst.video_pic_url});background-size:cover;"><img src="images/icn_play_big.png" alt="" class="icn_big_play">` +
             '<div class="pl_blue"></div></div></a>' +
             '<div id="video_description" class="video_description">' +
-            `<h2 class="heading_middle heading_video">${item["videos"][iVideoIdFirst]["video_name"]}</h2>` +
+            `<h2 class="heading_middle heading_video">${iVideoIdFirst.video_name}</h2>` +
             '<div class="razdelit_mid"></div>' +
-            `<div class="txt_author">${item["videos"][iVideoIdFirst]["video_author"]}</div>` +
-            `<p class="txt_std txt_video_desc">${item["videos"][iVideoIdFirst]["video_text"]}</p></div>`;
+            `<div class="txt_author">${iVideoIdFirst.video_author}</div>` +
+            `<p class="txt_std txt_video_desc">${iVideoIdFirst.video_text}</p></div>`;
         }) +
         $.each(item.videos, (iVideoId, aVideo) => {
           `<div class="video_preview preview_vebinar3_1 liteTooltip" style="background-image: url(../images/icn_play_small.png), url(${aVideo[iVideoId]["video_pic_url"]});opacity: 0.5;"></div>`;
