d1c3c5005cc15775d9b062f0cc061041d603ed2b
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-01 15:56:54 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -102,8 +102,8 @@ $.getJSON("https://video.gd.ru/event-type-2", function (data) {
         '<div class="line_razd blue"></div></div>' +
         '<div class="content">' +
         '<div class="block_video">' +
-        $.each(item.videos, (iVideoId, aVideo) => {
-          JSON.stringify(aVideo);
+        $.each(item.videos, (iVideoId, aVideo_item) => {
+          var aVideo = JSON.stringify(aVideo_item);
           if (iVideoIdFirst === "") {
             iVideoIdFirst = iVideoId;
             console.log(iVideoIdFirst, aVideo);
