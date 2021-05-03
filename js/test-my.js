eb83a74616d5b9455fbe3b6bdfff2af62ccd2f85
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-03 21:28:13 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -204,7 +204,6 @@ $(".section_vebirars").on("click", ".video_preview", function () {
   console.log("click");
   var previewVal = $(this).attr("data-tooltip");
   console.log(previewVal);
-
   // $("#vebinars .slick-current .heading_video").append(previewVal);
   $.getJSON("https://video.gd.ru/event-type-2", function (data) {
     $.each(data, function (i, item) {
@@ -212,10 +211,9 @@ $(".section_vebirars").on("click", ".video_preview", function () {
         $(this).filter((i) => {
           i.video_name == previewVal;
         });
+        console.log(item.videos);
       });
     });
-
-    console.log(data);
   });
 });
 
