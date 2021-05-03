c049f271a670a8f7c333353436db490681332b4e
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-03 18:05:03 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -198,30 +198,9 @@ $.getJSON("https://video.gd.ru/event-type-2", function (data) {
         '<div class="pl_show"></div></div></div></div>'
     );
   });
-
-  $.each(data, function (i, item) {
-    //Массив для видео вебинара после клика на превью
-    $.each(item.videos, (iVideoId, aVideo) => {
-      var video_name = aVideo.video_name;
-      iVideoIdCur = iVideoId;
-
-      console.log(iVideoIdCur.aVideo);
-      // $(video_name).click(() => {});
-
-      // result.push(
-      //   '<div class="main-slide-content">' +
-      //     `<a href=${aVideo.video_url} class="fancybox-media w-inline-block">` +
-      //     `<div class="video" style="background-color:rgba(63, 145, 229, 0.7);background-image: url(${aVideo.video_pic_url});background-size:cover;"><img src="images/icn_play_big.png" alt="" class="icn_big_play">` +
-      //     '<div class="pl_blue"></div></div></a>' +
-      //     '<div id="video_description" class="video_description">' +
-      //     `<h2 class="heading_middle heading_video">${aVideo.video_name}</h2>` +
-      //     '<div class="razdelit_mid"></div>' +
-      //     `<div class="txt_author">${aVideo.video_author}</div>` +
-      //     `<p class="txt_std txt_video_desc">${aVideo.video_text}</p></div></div>`
-      // );
-    });
-  });
 });
+
+
 
 //-------------------------------
 
