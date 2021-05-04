c9ad9d7df70d7b39316bf3241d139e9e0a0f2f21
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-04 14:40:54 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -202,6 +202,7 @@ $.getJSON("https://video.gd.ru/event-type-2", function (data) {
 //Контент слайдера (вебинары) после клика на превью
 $(".section_vebirars").on("click", ".video_preview", function () {
   var previewVal = $(this).attr("data-tooltip");
+  $(this).css("opacity", 0.5);
 
   $.getJSON("https://video.gd.ru/event-type-2", function (data) {
     $.each(data, function (i, item) {
@@ -241,11 +242,11 @@ $(".chapters_block2").slick({
   centerMode: true,
   dots: false,
   arrows: false,
-  asNavFor: ".section_vebirars",
+  asNavFor: "#conferenses",
 });
 
 //Слайдер для контента (выступления)
-$(".section_vebirars").slick({
+$("#conferenses").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
@@ -259,71 +260,5 @@ $.getJSON("https://video.gd.ru/event-type-1", function (data) {
   //Перебираем в цикле
   $.each(data, function (i, item) {
     console.log(item);
-    //Верстка для навигации
-    $(".chapters_block2").slick(
-      "slickAdd",
-      '<div class="conf_card conf1_card">' +
-        `<a href="#conferenses"><div class=conf1_img" style="background-image: url(${item2.video_type_pic_url});"></div></a>` +
-        '<div class="chapter_txt"><a href="#conferenses">' +
-        `<h3 class="heading_small">${item.video_type_name}</h3>` +
-        '<div class="razdelit_small"></div></a>' +
-        '<a href="#conferenses" class="link_look1 w-inline-block">' +
-        '<div class="txt_link_look">смотреть</div>' +
-        "</a>" +
-        `<a href="#conferenses" class="link_look1 active w-inline-block">` +
-        '<div class="txt_link_look">смотреть</div>' +
-        "</a></div></div>"
-    );
-
-    var iVideoIdFirstVy = "";
-    var resultVy = [];
-    var result_previewVy = [];
-
-    //Массив для видео выступление + описание
-    $.each(item.videos, (iVideoId, aVideo) => {
-      if (iVideoIdFirstVy === "") {
-        iVideoIdFirstVy = iVideoId;
-      } else {
-        return false;
-      }
-
-      resultVy.push(
-        '<div class="main-slide-content">' +
-          `<a href=${aVideo.video_url} class="fancybox-media w-inline-block">` +
-          `<div class="video" style="background-color:rgba(63, 145, 229, 0.7);background-image: url(${aVideo.video_pic_url});background-size:cover;"><img src="images/icn_play_big.png" alt="" class="icn_big_play">` +
-          '<div class="pl_blue"></div></div></a>' +
-          '<div id="video_description" class="video_description">' +
-          `<h2 class="heading_middle heading_video">${aVideo.video_name}</h2>` +
-          '<div class="razdelit_mid"></div>' +
-          `<div class="txt_author">${aVideo.video_author}</div>` +
-          `<p class="txt_std txt_video_desc">${aVideo.video_text}</p></div></div>`
-      );
-    });
-
-    //Массив для превью видео выступлений
-    $.each(item.videos, (iVideoId, aVideo) => {
-      result_previewVy.push(
-        `<div class="video_preview preview_vebinar3_1 liteTooltip" style="background-image: url(../images/icn_play_small.png), url(${aVideo.video_pic_url});opacity: 0.8;" data-tooltip="${aVideo.video_name}"></div>`
-      );
-    });
-
-    //Верстка для контента
-    $(".section_vebirars").slick(
-      "slickAdd",
-      '<div class="container-upr">' +
-        `<h1 class="header_std">${item.video_type_name}</h1>` +
-        '<div class="razdelit_upravl" id="vebirars_view">' +
-        `<div class="line_razd blue"></div><img src=${item.video_type_icon_url} width="29" alt="" class="icn_upravl">` +
-        '<div class="line_razd blue"></div></div>' +
-        '<div class="content">' +
-        '<div class="block_video">' +
-        resultVy.join("") +
-        '<div class="block_video_preview">' +
-        result_previewVy.join("") +
-        "</div>" +
-        '<div class="video_show_all liteTooltip">' +
-        '<div class="btn_arrow_right"><img src="images/icn_arrow_right.svg" alt="" class="arrow_bottom"></div></div></div>' +
-        '<div class="pl_show"></div></div></div></div>'
-    );
   });
 });
