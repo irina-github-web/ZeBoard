7e8a20af4a7e912b28fd66225650a882448ae43d
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-05 19:37:52 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -337,7 +337,7 @@ $.getJSON("https://video.gd.ru/event-type-1", function (data) {
     $("#conferenses").slick(
       "slickAdd",
       '<div class="container-upr">' +
-        `<h1 class="header_std">${item.video_type_name}</h1>` +
+        `<h1 class="header_std">${item.video_type_name} ${item.videos.video_year}</h1>` +
         '<div class="razdelit_upravl" id="vebirars_view">' +
         `<div class="line_razd blue"></div><img src=${item.video_type_icon_url} width="29" alt="" class="icn_upravl">` +
         '<div class="line_razd blue"></div></div>' +
