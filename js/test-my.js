fd1c1054e0d47548e5feb361b03ec77ae891d503
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-07 12:14:02 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -407,7 +407,7 @@ $("#conferenses").on("click", ".video_preview", function () {
 $("#conferenses").on("click", ".pag_item", function () {
   var currYear = $(this).attr("data-tooltip-year");
   console.log(currYear);
-  $(".conf_card").remove();
-  $(".container-upr").remove();
+  $("#conferenses .conf_card").remove();
+  $("#conferenses .container-upr").remove();
   getConferences(currYear);
 });
