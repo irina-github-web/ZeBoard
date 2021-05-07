c9dce191b417e630ba16e9360fd17f00febc18e8
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-07 21:11:53 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -404,7 +404,7 @@ $("#conferenses").on("click", ".video_preview", function () {
 });
 
 //Контент слайда после клика на год pagination
-$("#conferenses").on("click", ".pag_item", () => {
+$("#conferenses").on("click", ".pag_item", function() {
   var currYear = $(this).attr("data-tooltip-year");
   console.log(currYear);
   $(".chapters_block2 .conf_card").remove();
@@ -413,17 +413,17 @@ $("#conferenses").on("click", ".pag_item", () => {
 });
 
 //Show more button для видео-превью
-$(window).on("load", function () {
-  var previewsArr = $("#conferenses .slick-current .video_preview");
-  console.log(previewsArr);
-  if ($(previewsArr).length > 9) {
-    console.log("arr > 9");
-    $(previewsArr).slice(0, 9);
-    $("#conferenses .slick-current .video_show_all").css("display", "block");
-  }
-  $("body").on("click", ".video_show_all", () => {
-    console.log("click");
-    $("#conferenses .slick-current .video_preview:hidden").slice(0, 9).fadeIn();
-  });
-});
+// $(window).on("load", function () {
+//   var previewsArr = $("#conferenses .slick-current .video_preview");
+//   console.log(previewsArr);
+//   if ($(previewsArr).length > 9) {
+//     console.log("arr > 9");
+//     $(previewsArr).slice(0, 9);
+//     $("#conferenses .slick-current .video_show_all").css("display", "block");
+//   }
+//   $("body").on("click", ".video_show_all", () => {
+//     console.log("click");
+//     $("#conferenses .slick-current .video_preview:hidden").slice(0, 9).fadeIn();
+//   });
+// });
 // setTimeout(getMore, 5000);
