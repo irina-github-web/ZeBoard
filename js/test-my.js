492120ee702aa7147bd157199dc322aeabc7fae1
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-10 20:43:07 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -447,7 +447,7 @@ $("#conferenses").on("click", ".year_pagination", function () {
 
   if (currSlideIndex == 1) {
     console.log("if done");
-      // $(".chapters_block2 .slick-current").trigger("click");
+      $(".chapters_block2 .slick-current").trigger("click");
       $("#conferenses").slick("slickGoTo", currSlideIndex);
       // $("#conferenses").slick("refresh");
       
