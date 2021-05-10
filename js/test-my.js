c84fb2973ba78930f6b6264db0c4e88d1135855f
Irina Morozova <66619077+irina-github-web@users.noreply.github.com>
2021-05-10 15:11:01 +0300

    Update test-my.js

--- js/test-my.js
+++ js/test-my.js
@@ -449,7 +449,7 @@ $("#conferenses").on("click", ".year_pagination", function (e) {
 
   if (currSlideIndex == 1) {
     console.log("if done");
-    $(".chapters_block2 .slick-current")[0].click();
+    $(".chapters_block2 .slick-current")[0].trigger("click");
   }
   // loadMoreVideos();
 });
